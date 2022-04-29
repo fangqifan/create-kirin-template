7a2449e618a3d4644cf811771122532bdb6756c5
fangqifan@kirinsoft.cn <fangqifan@kirinsoft.cn>
2022-04-30 02:50:08 +0800

    fix

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 // import { dirname } from 'path'
 // import { fileURLToPath } from 'url'
 
-var fs = require('fs')
+
 // const { dirname }  = require('path')
 // const { fileURLToPath } = require(fs)
 
 function main() {
-    if (!fs.existsSync('package.json')) {
-        console.log('package.json not found,Please run "npm init" at first.')
-        return
-    }
+    console.log('sadfsadfsadf')
+    // var fs = require('fs')
 
-    fs.appendFileSync('123.txt', __dirname)
-    fs.appendFileSync('123.txt', __filename)
+    // if (!fs.existsSync('package.json')) {
+    //     console.log('package.json not found,Please run "npm init" at first.')
+    //     return
+    // }
+
+    // fs.appendFileSync('123.txt', __dirname)
+    // fs.appendFileSync('123.txt', __filename)
     // fs.appendFileSync('123.txt', dirname(fileURLToPath(import.meta.url)))
     //console.log(dirname(fileURLToPath(import.meta.url)))
 
