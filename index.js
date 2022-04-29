23b10cb595983bb955b7399ee55f47cbe8b1dfea
fangqifan@kirinsoft.cn <fangqifan@kirinsoft.cn>
2022-04-30 02:25:30 +0800

    fix

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import fs from 'fs'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-(async function () {
-
+function main() {
     if (!fs.existsSync('package.json')) {
         console.log('package.json not found,Please run "npm init" at first.')
         return
@@ -16,4 +15,6 @@ import { fileURLToPath } from 'url'
     // if (!fs.existsSync('src/index.ts')) {
     //     fs.copyFileSync('src')
     // }
-})()
+}
+
+main()
